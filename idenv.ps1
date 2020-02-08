1e2322a8c03f5479a97c0e9e435a2d629aea64f1
Osamu Morinaga <7270229+o-samurai@users.noreply.github.com>
2020-02-08 19:54:52 +0900

    To run idenv without exit powershell.

--- idenv.ps1
+++ idenv.ps1
@@ -2,13 +2,6 @@ function main(){
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 }
 
-$err=0
 $ErrorActionPreference = "stop"
 
-try{
-    $err = main
-} catch [Exception] {
-    $err = 1
-}
-
-exit $err
+main
